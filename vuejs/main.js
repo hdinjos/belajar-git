251537f25722c90368a5f4d7def9ec0c709e4d46
hdinjos <hdinjos@gmail.com>
2021-04-16 11:15:41 +0700

    commit 2

--- vuejs/main.js
+++ vuejs/main.js
@@ -9,17 +9,32 @@ Vue.component('bagian-input', {
   template: `<div style="border: 1px solid blue;">
     <input type="number" v-model="input" placeholder="isi nomor apa aja"/>
     <p>{{input}}</p>
-    <bagian-btn></bagian-btn>
+    <bagian-btn v-bind:name="input"></bagian-btn>
     </div>`
 })
 
 Vue.component('bagian-btn', {
+  data: () => {
+    return {
+      val: ''
+    }
+  },
   template: `
   <div style="border: 1px solid red">
-    <button>Tambah 2</button>
-    <button>Kurang 3</button>
+    <button @click="incrementVal">Tambah berdasarkan nilai input</button>
+    <p>{{val}}</p>
   </div>
-  `
+  `,
+  props: ['name'],
+  methods: {
+    incrementVal() {
+      this.val = this.name;
+      let num = this.val
+      parseInt(num) += 1;
+
+
+    }
+  }
 })
 
 var vm = new Vue({
