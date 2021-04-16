ca2a5ae2029d4da3a2dd7b62f1c3ad8035e17715
hdinjos <hdinjos@gmail.com>
2021-04-16 10:57:57 +0700

    compoent

--- vuejs/main.js
+++ vuejs/main.js
@@ -1,21 +1,31 @@
 console.log("halo");
 
-var data = { a: 1 }
+Vue.component('bagian-input', {
+  data: () => {
+    return {
+      input: ''
+    }
+  },
+  template: `<div style="border: 1px solid blue;">
+    <input type="number" v-model="input" placeholder="isi nomor apa aja"/>
+    <p>{{input}}</p>
+    <bagian-btn></bagian-btn>
+    </div>`
+})
+
+Vue.component('bagian-btn', {
+  template: `
+  <div style="border: 1px solid red">
+    <button>Tambah 2</button>
+    <button>Kurang 3</button>
+  </div>
+  `
+})
+
 var vm = new Vue({
   el: '#example',
   data: {
     halo: "okeeee",
     num: 6,
-    input: ''
   },
-  methods: {
-    incrementNum() {
-      let number = parseInt(this.input);
-      this.num += number;
-      console.log(typeof (this.num));
-    },
-    decrementNum() {
-      this.num -= 3;
-    }
-  }
 })
